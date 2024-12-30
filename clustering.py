720f330ccf6cb2d11c59c059409b82d53f65d9f9
Nikunj Kaushik <kaushiknikunj1@gmail.com>
2024-12-30 19:04:25 +0530

    Create clustering.py

---
+++ clustering.py
@@ -0,0 +1,13 @@
+#installing basic libraries etc.
+
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+!pip install numpy pandas seaborn matplotlib textblob nltk wordcloud sklearn corpus vaderSentiment
